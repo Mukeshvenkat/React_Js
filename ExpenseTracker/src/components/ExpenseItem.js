466a5adae674d316e608e3ef792def812c4cfef5
MukeshVenkat <mukiv0802@gmail.com>
2022-08-09 12:22:46 +0530

    Multiple components are created and data is shared

--- ExpenseTracker/src/components/ExpenseItem.js
+++ ExpenseTracker/src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpensiveItem(props) {
+function ExpenseItem(props) {
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
@@ -12,4 +13,4 @@ function ExpensiveItem(props) {
     );
 }
 
-export default ExpensiveItem;
+export default ExpenseItem;
