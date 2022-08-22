56f7d842cf7e6146fcbc588683ed5d9bc79be627
MukeshVenkat <mukiv0802@gmail.com>
2022-08-22 09:51:13 +0530

    Button module & Add User logic added!

--- UserEntry/src/components/User/AddUser.js
+++ UserEntry/src/components/User/AddUser.js
@@ -1,19 +1,31 @@
+import { useState } from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault(); //Inorder to prevent the page from reloading when form is submitted
+    console.log( enteredUserName ,enteredAge)
   };
 
+  const userNameChangeHandler = event => {
+    setEnteredUserName(event.target.value);
+  };
+  const ageChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangeHandler}/>
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
