93ee3029caef34dd372ab8d3d5b8686a4e771359
MukeshVenkat <mukiv0802@gmail.com>
2022-08-29 22:42:08 +0530

    CartContext and context provider added.

---
+++ FoodOrderApp/src/store/cart-context.js
@@ -0,0 +1,12 @@
+const CartContext = React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {
+
+    },
+    removeItem: (id) => {
+
+    }
+});
+
+export default CartContext;
