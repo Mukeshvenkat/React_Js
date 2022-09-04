da1a112b02e99f3f06b78b915bf8a0cce0faea0c
MukeshVenkat <mukiv0802@gmail.com>
2022-09-04 12:58:39 +0530

    UseState and UseRef used to handle input text data.

--- FormSample/src/components/SimpleInput.js
+++ FormSample/src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    //Dont Use this because It Manipulates the DOM Directly
+    // nameInputRef.current.value = '';
+
+    setEnteredName("");
+  };
+
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control" onSubmit={formSubmissionHandler}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
