618c67664a948da5b13e677e2ae651f7a4d3504b
MukeshVenkat <mukiv0802@gmail.com>
2022-09-05 20:13:29 +0530

    Redux added for login and logout.

--- Redux_Sample/src/components/Counter.js
+++ Redux_Sample/src/components/Counter.js
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
-import { counterActions } from "../store";
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   //useSelectore creates and manages the subscription to the redux store
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
