85ac05bb460331c8b05a3890e5e237f92d5a3b5c
MukeshVenkat <mukiv0802@gmail.com>
2022-09-10 11:42:56 +0530

    Switch, exact, Redirect, RoutingParams are added.

--- Router_Sample/src/pages/Products.js
+++ Router_Sample/src/pages/Products.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-    return (<h1>Products Page is Loaded!</h1>);
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li><Link to='/products/p1'>The Fellowship Of The Ring</Link></li>
+        <li><Link to='/products/p2'>Two Towers</Link></li>
+        <li><Link to='/products/p3'>The Return Of The Kings</Link></li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
