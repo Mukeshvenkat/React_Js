52751103ccb4308110d5bc4cb8030256c8a6edb7
MukeshVenkat <mukiv0802@gmail.com>
2022-09-12 19:23:54 +0530

    Basic Routing, Redirect & Nested Routes are added.

--- Router_Notes_App/src/App.js
+++ Router_Notes_App/src/App.js
@@ -1,8 +1,24 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes'></Redirect>
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuotes />
+      </Route>
+    </Switch>
   );
 }
 
