62815cd5e7d8061d6e3ee2816177ab0b356270de
MukeshVenkat <mukiv0802@gmail.com>
2022-09-12 19:48:20 +0530

    Dummy data and Add a new quote also added.

--- Router_Notes_App/src/App.js
+++ Router_Notes_App/src/App.js
@@ -1,24 +1,27 @@
 import { Redirect, Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layput";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuotes from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/quotes'></Redirect>
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuotes />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes"></Redirect>
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuotes />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- Router_Notes_App/src/pages/AllQuotes.js
+++ Router_Notes_App/src/pages/AllQuotes.js
@@ -1,5 +1,19 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTE = [
+  {
+    id: "q1",
+    author: "Mukesh",
+    text: "Mukesh is learning React!",
+  },
+  {
+    id: "q2",
+    author: "Mukesh Mind",
+    text: "Mukesh is trying everything to avoid learning React!",
+  },
+];
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTE} />;
 };
 
 export default AllQuotes;
